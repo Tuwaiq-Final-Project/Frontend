8fc89fe678b4865c1f49938137f21815c6560e5e
S-MW <72529306+S-MW@users.noreply.github.com>
2021-12-15 13:36:09 +0300

    Apply SignIn , SignUp components & Able to routing with them

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import {Routes, Route} from "react-router-dom"
+
+// All components
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+import SignIn from './components/SignIn/SignIn';
+import SignUp from './components/SignUp/SignUp';
 
 function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/Sign-in" element={<SignIn/>}/>
+        <Route path="/Sign-up" element={<SignUp/>}/>
+      </Routes>
     </>
   );
 }
